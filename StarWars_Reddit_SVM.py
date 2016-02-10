961d6df00b0bbe91b84fb221b3e8b1df5baf8a83
Zabriskie <patrickzabriskie@MacBook-Pro-3.local>
2016-02-10 11:58:45 -0500

    Update
    Update

--- StarWars_Reddit_SVM.py
+++ StarWars_Reddit_SVM.py
@@ -5,7 +5,7 @@ from the "movies" subreddit or the "StarWars" subreddit based on the number of t
 appear in that body. The classification is done through Linear SVM. Linear SVM creates a linear classifier and 
 assigns weights to each category; the larger the magnitude of the weight, the more significant the category 
 is to classification. The main goal is to determine the most significant category in classifying these bodies. 
-The output from this code will two data tables: a data table showing different Star Wars categories, and the
+The output from this code will be two data tables: a data table showing different Star Wars categories, and the
 number of times words from those categories appear in each body; and a smaller data table at the end, showing 
 the weights obtained from Linear SVM, and an overall proportion that was correctly classified through Linear SVM. 
 The categories will be ranked according to weight, starting with the weight of largest magnitude."""
