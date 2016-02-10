98b11a3f95fc939c4dec51148b8f615cb5cc30d9
Zabriskie <patrickzabriskie@MacBook-Pro-3.local>
2016-02-10 11:45:45 -0500

    Update
    Update

--- Reddit_SVM.py
+++ Reddit_SVM.py
@@ -1,6 +1,14 @@
 """Star Wars-themed posts can be found in different subreddits. This Python script
-performs linear classification. It reads in reddit bodies from two subreddits where Star Wars-themed posts can occur, "movies" and "StarWars", and attempts to classify whether or not the reddit body came from the "movies" subreddit or the "StarWars" subreddit based on the number of times words of certain categories appear in that body. The classification is done through Linear SVM. Linear SVM creates a linear classifier and assigns weights to each category; the larger the magnitude of the weight, the 
-more significant the category is to classification. The main goal is to determine the most significant category in classifying these bodies. The output from this code will two data tables: a data table showing different Star Wars categories, and the number of times words from those categories appear in each body; and a smaller data table at the end, showing the weights obtained from Linear SVM, and an overall proportion that was correctly classified through Linear SVM. The categories will be ranked according to weight, starting with the weight of largest magnitude."""
+performs linear classification. It reads in reddit bodies from two subreddits where Star Wars-themed 
+posts can occur, "movies" and "StarWars", and attempts to classify whether or not the reddit body came 
+from the "movies" subreddit or the "StarWars" subreddit based on the number of times words of certain categories 
+appear in that body. The classification is done through Linear SVM. Linear SVM creates a linear classifier and 
+assigns weights to each category; the larger the magnitude of the weight, the more significant the category 
+is to classification. The main goal is to determine the most significant category in classifying these bodies. 
+The output from this code will two data tables: a data table showing different Star Wars categories, and the
+number of times words from those categories appear in each body; and a smaller data table at the end, showing 
+the weights obtained from Linear SVM, and an overall proportion that was correctly classified through Linear SVM. 
+The categories will be ranked according to weight, starting with the weight of largest magnitude."""
 
 import numpy as np
 import pandas as pd
